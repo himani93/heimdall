fc6dcf655f78afc6814f70ebcc9cf8ffe70b6767
Himani Agrawal <himani93@gmail.com>
2016-10-10 18:04:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Logger for vapor
---
+++ sources/VaporLogger.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class VaporLogger {
+
+	public func defaultLogger() -> String {
+		print("I am default Logger")
+		return "I am default Logger"
+	}
+
+}
