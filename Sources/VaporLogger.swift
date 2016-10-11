1585c947acbd9e0cbc1e3f348c07b93cb29a3b18
Himani Agrawal <himani93@gmail.com>
2016-10-11 23:09:07 +0530

    added middleware logger

--- Sources/VaporLogger.swift
+++ Sources/VaporLogger.swift
@@ -5,12 +5,23 @@ public class VaporLogger {
 	public init () {
 
 	}
-	public func defaultLogger() -> ((Request) -> Void) {
-		print("I am default Logger")
-		func requestLogger(req: Request) {
-			print(req)
+	public func defaultLogger(a: String) -> ((Request) -> String) {
+		print("I am default Logger \(a)")
+		func requestLogger(request: Request) -> String {
+			print("\(request)")
+			return "request logged!"
 		}
 		return requestLogger
 	}
-
+	public func middlewareLogger() -> ((Request, Responder) throws -> Response) {
+		func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+	    	print("\(request)")
+		    return try next.respond(to: request)
+		}
+		return respond
+	}
+	public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+    	print("\(request)")
+	    return try next.respond(to: request)
+	}
 }
