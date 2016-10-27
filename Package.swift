035419fb3d78483d667e9e42d1a32df108be86b7
Himani Agrawal <himani93@gmail.com>
2016-10-27 11:24:09 +0530

    project renamed to heimdall

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporLogger",
+    name: "heimdall",
     dependencies: [
 	.Package(url: "https://github.com/vapor/engine.git", majorVersion: 1),
 	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
