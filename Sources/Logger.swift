b4f1ee7c8596755e1df3256ebc5eaa124787a676
Himani Agrawal <himani93@gmail.com>
2016-11-21 19:39:33 +0530

    Identation

--- Sources/Logger.swift
+++ Sources/Logger.swift
@@ -1,6 +1,11 @@
 import Foundation
 import HTTP
 
+// TODO: compare with morgan
+// TODO: example project
+// TODO: check ubuntu
+
+
 public class Logger: Middleware {
     
     enum fileError: Error {
@@ -11,12 +16,12 @@ public class Logger: Middleware {
     }
     
     var file: String
-	var format: String
-
+    var format: String
+    
     public init(format: String, file: String) {
-		self.format = format
+        self.format = format
         self.file = file
-	}
+    }
     
     public convenience init() {
         self.init(format: "combined", file: "logs.txt")
@@ -28,55 +33,55 @@ public class Logger: Middleware {
     
     public convenience init(format: String) {
         self.init(format: format, file: "logs.txt")
-	}
-
-	deinit {
+    }
     
-	}
-
-	public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
-		let requestInTime: Date = Date()
-		let remoteAddr = request.peerAddress!.address()
-		let method = request.method
-		let url = request.uri
-
-		let response = try next.respond(to: request)
-
-		let responseTime = Date().timeIntervalSince(requestInTime)
-		let status = response.status.statusCode
-		let httpVersion = "HTTP/\(response.version.major).\(response.version.minor)"
-
-		// TODO: set remoteUser if user authenticated
-		let remoteUser = "-"
-		// TODO: check how to get reponse[content-length]
-		let responseContentLength = "-"
-
-		var referer = "-"
-		if request.headers["referer"] != nil {
-			referer = (request.headers["referer"])!
-		}
-
-		var userAgent = "-"
-		if request.headers["User-Agent"] != nil {
-			userAgent = (request.headers["User-Agent"])!
-		}
-
-		var content: String = {
-			switch format {
-				case "combined":
-					return "\(remoteAddr)\t-\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\(responseContentLength)\t\"\(referer)\"\t\"\(userAgent)\""
-				case "common":
-					return "\(remoteAddr)\t-\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\\(responseContentLength)"
-				case "dev":
-					return "\(method)\t\(url)\t\(status)\t\(responseTime) ms\t\(responseContentLength)"
-				case "short":
-					return "\(remoteAddr)\t\(remoteUser)\t\(method)\t\(url)\t\(httpVersion)\t\(status)\t\(responseContentLength)\t\(responseTime) ms"
-				case "tiny":
-					return "\(method)\t\(url)\t\(status)\t\(responseContentLength)\t\(responseTime) ms"
-				default:
-					return "\(remoteAddr)\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\\(responseContentLength)\t\"\(referer)\"\t\"(userAgent)\""
-			}
-		}()
+    deinit {
+        
+    }
+    
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        let requestInTime: Date = Date()
+        let remoteAddr = request.peerAddress!.address()
+        let method = request.method
+        let url = request.uri
+        
+        let response = try next.respond(to: request)
+        
+        let responseTime = Date().timeIntervalSince(requestInTime)
+        let status = response.status.statusCode
+        let httpVersion = "HTTP/\(response.version.major).\(response.version.minor)"
+        
+        // TODO: set remoteUser if user authenticated
+        let remoteUser = "-"
+        // TODO: check how to get reponse[content-length]
+        let responseContentLength = "-"
+        
+        var referer = "-"
+        if request.headers["referer"] != nil {
+            referer = (request.headers["referer"])!
+        }
+        
+        var userAgent = "-"
+        if request.headers["User-Agent"] != nil {
+            userAgent = (request.headers["User-Agent"])!
+        }
+        
+        var content: String = {
+            switch format {
+            case "combined":
+                return "\(remoteAddr)\t-\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\(responseContentLength)\t\"\(referer)\"\t\"\(userAgent)\""
+            case "common":
+                return "\(remoteAddr)\t-\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\\(responseContentLength)"
+            case "dev":
+                return "\(method)\t\(url)\t\(status)\t\(responseTime) ms\t\(responseContentLength)"
+            case "short":
+                return "\(remoteAddr)\t\(remoteUser)\t\(method)\t\(url)\t\(httpVersion)\t\(status)\t\(responseContentLength)\t\(responseTime) ms"
+            case "tiny":
+                return "\(method)\t\(url)\t\(status)\t\(responseContentLength)\t\(responseTime) ms"
+            default:
+                return "\(remoteAddr)\t\(remoteUser)\t[\(convertDateToCLF(date: requestInTime))]\t\"\(method)\t\(url)\t\(httpVersion)\"\t\(status)\t\\(responseContentLength)\t\"\(referer)\"\t\"(userAgent)\""
+            }
+        }()
         content = content + "\n"
         
         do {
@@ -84,14 +89,14 @@ public class Logger: Middleware {
         } catch {
             print("Heimdall could not write to file")
         }
-		return response
-	}
-
-	func convertDateToCLF(date: Date) -> String {
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "d/MMM/y:H:m:s Z"
-		return dateFormatter.string(from: date)
-	}
+        return response
+    }
+    
+    func convertDateToCLF(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d/MMM/y:H:m:s Z"
+        return dateFormatter.string(from: date)
+    }
     
     func convertStringToData(content: String) -> Data? {
         return content.data(using: String.Encoding.utf8)
--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -2,33 +2,33 @@ import Vapor
 
 public final class Provider: Vapor.Provider {
     public let logger: Logger
-
+    
     public func beforeRun(_: Vapor.Droplet) {
     }
-
+    
     public init(config: Config) throws {
         logger = Logger()
     }
-
-    public init () {
+    
+    public init() {
         logger = Logger()
     }
-
-    public init (format: String) {
+    
+    public init(format: String) {
         logger = Logger(format: format)
     }
     
-    public init (file: String) {
+    public init(file: String) {
         logger = Logger(file: file)
     }
     
-    public init (format: String, file: String) {
+    public init(format: String, file: String) {
         logger = Logger(format: format, file: file)
     }
     
     public func afterInit(_ drop: Droplet) {
     }
-
+    
     public func boot(_ drop: Droplet) {
         drop.middleware.append(logger)
     }
