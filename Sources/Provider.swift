923b9eb3c658c2e30559344d45b5bcdcc349f9b3
Himani Agrawal <himani93@gmail.com>
2016-12-04 12:40:00 +0530

    get setting from config file

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -2,12 +2,42 @@ import Vapor
 
 public final class Provider: Vapor.Provider {
     public let logger: Logger
+    enum ConfigError: Error {
+        case configNotFound
+    }
     
     public func beforeRun(_: Vapor.Droplet) {
     }
     
     public init(config: Config) throws {
-        logger = Logger()
+        guard let heimdallConfig = config["heimdall"]?.object else {
+            throw ConfigError.configNotFound
+        }
+
+        guard
+            let file = heimdallConfig["file"]?.string,
+            let format = heimdallConfig["format"]?.string else {
+                self.logger = Logger()
+                return
+        }
+
+        let formatType: LogType = {
+            switch format {
+            case "common":
+                return .common
+            case "dev":
+                return .dev
+            case "short":
+                return .short
+            case "tiny":
+                return .tiny
+            default:
+                return .combined
+            }
+        }()
+
+        // TODO: call initializer based on existence of file and format
+        self.logger = Logger(format: formatType)
     }
     
     public init() {
